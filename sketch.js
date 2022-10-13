49fa64c27fa4cc5f22742aee81121f01b65cf207
winnie-woo <hanxuwu0428@gmail.com>
2022-10-12 23:37:31 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,7 +1,44 @@
+//My Bunnies
+
+let Bunnies = [{
+  name:"Spaghetti",
+  color:"yellow"
+},{
+  name:"Chocolate",
+  color:"brown"
+},{
+  name:"Pizza",
+  color:"orange"
+},{
+  name:"Marshmallow",
+  color:"white"
+},{
+  name:"Tiramisu",
+  color:"beige"
+},{
+  name:"Bread",
+  color:"sesame"
+},];
+
+let randomIndex;
+
 function setup() {
     createCanvas(400, 400);
+    background(255, 255, 183);
+
   }
   
   function draw() {
-    background(220);
+    
   }
+
+function mousePressed(){
+  background(random(200,255));
+  randomIndex= int(random(Bunnies.Length));
+  //console.log(Bunnies.Length);
+  //console.log(Bunnies[int(random(Bunnies.length))].name);
+  //console.log(Bunnies[randomIndex].name);
+  text(Bunnies[randomIndex].name,50,50);
+  Bunnies.splice(randomIndex,1);
+
+}
